c9818494c2ca6ea616d3fd83c6568fee5202154a
Michael X. Grey <grey@osrfoundation.org>
2018-01-10 11:50:06 -0800

    Use the correct export macro definition

--- include/ignition/rndf/Helpers.hh
+++ include/ignition/rndf/Helpers.hh
@@ -22,42 +22,7 @@
 #include <cstring>
 #include <string>
 
-/// \def IGNITION_RNDF_VISIBLE
-/// Use to represent "symbol visible" if supported
-
-/// \def IGNITION_RNDF_HIDDEN
-/// Use to represent "symbol hidden" if supported
-
-#if defined BUILDING_STATIC_LIBS
-  #define IGNITION_RNDF_VISIBLE
-  #define IGNITION_RNDF_HIDDEN
-#else
-  #if defined __WIN32 || defined __CYGWIN__
-    #ifdef BUILDING_DLL
-      #ifdef __GNUC__
-        #define IGNITION_RNDF_VISIBLE __attribute__ ((dllexport))
-      #else
-        #define IGNITION_RNDF_VISIBLE __declspec(dllexport)
-      #endif
-    #else
-      #ifdef __GNUC__
-        #define IGNITION_RNDF_VISIBLE __attribute__ ((dllimport))
-      #else
-        #define IGNITION_RNDF_VISIBLE __declspec(dllimport)
-      #endif
-    #endif
-    #define IGNITION_RNDF_HIDDEN
-  #else
-    #if __GNUC__ >= 4
-      #define IGNITION_RNDF_VISIBLE __attribute__ ((visibility ("default")))
-      #define IGNITION_RNDF_HIDDEN  __attribute__ ((visibility ("hidden")))
-    #else
-      #define IGNITION_RNDF_VISIBLE
-      #define IGNITION_RNDF_HIDDEN
-    #endif
-  #endif
-// BUILDING_STATIC_LIBS
-#endif
+#include <ignition/rndf/Export.hh>
 
 namespace ignition
 {
